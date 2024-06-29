9860921c22e3a1d55d6c9680f5fc68bf45894dbe
Mohd Hamza Abbasi <141723368+mohdhamzaabbasi@users.noreply.github.com>
2024-06-29 15:30:17 +0530

    Time: 4 ms (19.72%), Space: 46.9 MB (55.77%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) 
     {
-        int res[]={1,number.length};
+        int res[]={0,numbers.length-1};
         while(res[0]<=res[1])
         {
             if(numbers[res[0]]+numbers[res[1]]>target)
@@ -17,6 +17,8 @@ class Solution {
                 break;
             }
         }
+        res[0]=res[0]+1;
+        res[1]=res[1]+1;
         return res;
     }
 }
